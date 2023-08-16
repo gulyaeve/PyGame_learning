11b236782b64c61ee0634a971c00d3ceb98224ae
gulyaeve <baddman@list.ru>
2023-08-16 13:31:12 +0300

    Fixed moved rectangle

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ import pygame
 import sys
 
 pygame.init()
-
 screen = pygame.display.set_mode((640, 480))
-
 rectangle = pygame.Rect(50, 50, 100, 50)
 
 
@@ -16,8 +14,11 @@ while True:
             pygame.quit()
             sys.exit()
 
-        if event.type == pygame.KEYUP:
-            rectangle.move_ip(0, -5)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                rectangle.move_ip(0, -5)
+            if event.key == pygame.K_DOWN:
+                pass
 
     pygame.draw.rect(screen, "white", rectangle, 0)
 
