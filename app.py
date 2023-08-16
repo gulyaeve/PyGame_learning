89f52fc263437baa53b25a4827538129b78297c9
gulyaeve <baddman@list.ru>
2023-08-16 13:26:04 +0300

    Added moved rectangle

--- app.py
+++ app.py
@@ -6,12 +6,19 @@ pygame.init()
 screen = pygame.display.set_mode((640, 480))
 
 rectangle = pygame.Rect(50, 50, 100, 50)
-pygame.draw.rect(screen, "white", rectangle, 0)
 
 
 while True:
-    pygame.display.flip()
+    screen.fill("black")
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+        if event.type == pygame.KEYUP:
+            rectangle.move_ip(0, -5)
+
+    pygame.draw.rect(screen, "white", rectangle, 0)
+
+    pygame.display.flip()
