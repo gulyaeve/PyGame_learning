1eb7265b4b71742d149c5abdbd450d83fd6d7f23
gulyaeve <baddman@list.ru>
2023-08-16 13:16:00 +0300

    Added colored rectangle

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ pygame.init()
 
 screen = pygame.display.set_mode((640, 480))
 
+rectangle = pygame.Rect(50, 50, 100, 50)
+pygame.draw.rect(screen, "white", rectangle, 0)
+
 
 while True:
+    pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
